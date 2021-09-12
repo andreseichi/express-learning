8ad9809828214f8a1fad06d633d7970aee55398d
André Tashiro <andre_seichi@hotmail.com>
2021-09-12 12:31:27 -0300

    data

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express = require('express');
+import express from 'express';
+
+const importData = require('./data.json');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => {
+  res.send(importData);
 });
 
 app.listen(port, () => {
